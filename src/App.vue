840a073e2ae54efaa425dd4880654a3fb6095cdd
South-CreaWeb <south-creaweb@outlook.fr>
2026-04-18 18:11:40 +0200

    Add features: Delete,Filter,Done visual effect

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, computed } from "vue"
 
 type Tasks = {
   id: string,
@@ -9,8 +9,11 @@ type Tasks = {
 
 const task = ref<string>("")
 const tasks = ref<Tasks[]>([])
+const currentStatus = ref<"all" | "pending" | "done">("all")
 
 function addTask() {
+
+  if(!task.value.trim()) return
 
   const newTask = {
     id: crypto.randomUUID(),
@@ -23,6 +26,27 @@ function addTask() {
   task.value = ""
 }
 
+function deleteTask(id: string) {
+  tasks.value = tasks.value.filter(task => task.id !== id)
+}
+
+const filteredTasks = computed(() => {
+
+  if(currentStatus.value === "all") {
+    return tasks.value
+  }
+
+  if(currentStatus.value === "pending") {
+    return tasks.value.filter(task => !task.done)
+  }
+
+  if(currentStatus.value === "done") {
+    return tasks.value.filter(task => task.done)
+  }
+
+  return tasks.value
+})
+
 </script>
 
 
@@ -34,9 +58,9 @@ function addTask() {
   <section class="add_task">
     <h2>Add Task</h2>
     <div class="filter_task">
-      <button>All</button>
-      <button>Pending</button>
-      <button>done</button>
+      <button @click="currentStatus = 'all'">All</button>
+      <button @click="currentStatus = 'pending'">Pending</button>
+      <button @click="currentStatus = 'done'">done</button>
     </div>
     <input v-model="task" type="text">
     <button @click="addTask">Add</button>
@@ -44,13 +68,17 @@ function addTask() {
 
   <section class="display_tasks">
     <ul>
-      <li v-for="task in tasks" :key="task.id">
+      <li v-for="task in filteredTasks" :key="task.id" :class="{ disable: task.done }">
+        <input v-model="task.done" type="checkbox">
         {{ task.text }}
+        <button @click="deleteTask(task.id)">Delete</button>
       </li>
     </ul>
   </section>
 </template>
 
 <style>
-
+.disable {
+  text-decoration: line-through;
+}
 </style>
